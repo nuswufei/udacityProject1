abb43ecc839a808dfac2245e6ba351979da819fc
nuswufei <nuswufei@gmail.com>
2016-01-17 20:04:00 -0800

    class  3 finished

---
+++ app/src/main/java/com/sjsu/weathhelloworld2/SettingActivity.java
@@ -0,0 +1,46 @@
+package com.sjsu.weathhelloworld2;
+
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SettingActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+    final static String tag = SettingActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String stringValue = newValue.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(stringValue);
+            if (index >= 0) {
+                preference.setSummary(listPreference.getEntries()[index]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+        return true;
+    }
+}
